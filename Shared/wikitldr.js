e46d7967a4f2409973b60044522ff32e29c16dd8
Steven Wittens <steven@acko.net>
2010-12-25 03:19:13 -0800

    Tweak EO parser.

--- Shared/wikitldr.js
+++ Shared/wikitldr.js
@@ -278,7 +278,7 @@ if (document.title == 'Cable Viewer' && ($('div.paginator').length == 0)) {
 //      [ /^[A-Z]+: (((?!\s+\n)[^\n]+\n)+)/m, { 1: 'title' } ],
       [ /^SUBJECT: (((?!\s+\n|[A-Z][a-z]|(REF|TAGS):?)[^\n]{30,}\n)*((?!\s+\n|[A-Z][a-z]|(REF|TAGS):?)[^\n]+\n))/m, { 1: 'title' } ],
       [ /^TAGS:? (((?!\s+\n|[A-Z][a-z]|(REF|SUBJECT):?)[^\n]{30,}\n)*((?!\s+\n|[A-Z][a-z]|(REF|SUBJECT):?)[^\n]+\n))/m, { 1: 'tags' } ],
-      [ /^E\.O\. [0-9]+: DECL: ([^\n]+)\n/m, { 1: 'declassified' } ],
+      [ /^E\.?O\.? [0-9]+:? DECL: ([^\n]+)\n/m, { 1: 'declassified' } ],
     ];
     
     // Apply matching rules to body text.
@@ -390,7 +390,7 @@ if (document.title == 'Cable Viewer' && ($('div.paginator').length == 0)) {
       // Tags lines (remove).
       [ /^TAGS:? (((?!\s+\n|[A-Z][a-z]|(REF|SUBJECT):)[^\n]{30,}\n)*((?!\s+\n|[A-Z][a-z]|(REF|SUBJECT):?)[^\n]+\n))/m, '' ],
       // Declassification date (remove).
-      [ /^E\.O\. [0-9]+: (N\/A|DECL: ([^\n]+))(?=\n)/m, '' ],
+      [ /^E\.?O\.? [0-9]+:? (N\/A|DECL: ([^\n]+))(?=\n)/m, '' ],
       // Raw classification.
       [ /^(UNCLAS|C O N F I D E N T I A L|S E C R E T)( SECTION [0-9]+(\.[0-9]+)? OF [0-9]+)? [A-Z ]+ [0-9]+\s*$/m, '' ],
       // Continued cable
